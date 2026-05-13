24f6609686380c7909754d74fe64dbb10b2574e0
sanbi07 <sanbishah5@gmail.com>
2026-05-13 22:21:59 +0545

    added dic.py

---
+++ dic.py
@@ -0,0 +1,27 @@
+'''cart1={'ram': 9841740652}
+cart1['shyam']=9841740652
+
+print(cart1)
+cart1.update({'hari': 98041740652,'sita': 9803514140})
+print(cart1)
+
+cart1={'ram': 9803514140}
+print(cart1.egt('shyam','user not found'))
+
+#pop key and value pair remove, pop item ....
+cart1={'ram':903514140,'shyam': 9803514140}
+'''
+
+student_marks={'ram':55,'shyam':44,'hari':77}
+for i in student_marks:
+    print(i)
+for i in student_marks.keys():
+    print(i)
+for i in student_marks.values():
+    print(i)
+for i in student_marks.items():
+    print(i)
+for i,j in student_marks.items():
+    print(i,j)
+ 
+ 
